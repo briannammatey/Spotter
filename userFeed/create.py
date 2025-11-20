41b81c684a5e6f8032693748788a454cc094b523
Brianna <briianna@bu.edu>
2025-11-19 22:53:32 -0500

    Changes to improve spotter

---
+++ userFeed/create.py
@@ -0,0 +1,20 @@
+## start the challenge
+# if button press go to new screen
+# enters challenge details, and descriptions
+# okay we can create the challenge,
+
+
+def start():
+    ## maybe a method if a person presses start
+
+    pass
+
+def enterInfo():
+    ## asks the individual information
+    pass
+
+def validInfo():
+    pass
+
+def submitChallenge():
+    pass
