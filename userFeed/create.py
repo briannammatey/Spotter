7b685d4b37d9136beaccf30f8491012f2a6bb7f3
Brianna <briianna@bu.edu>
2025-11-20 11:21:30 -0500

    changes to frontend, now working on logWorkout

--- userFeed/create.py
+++ userFeed/create.py
@@ -5,16 +5,18 @@
 
 
 def start():
-    ## maybe a method if a person presses start
-
+    ## maybe a method if a person presses star5
     pass
 
-def enterInfo():
-    ## asks the individual information
-    pass
-
-def validInfo():
+def dateInfo():
+    # gets info from frontend
+    ## Checks if the Date is valid
+    # Checks include if start date is before end date, or
+    # if its before the date currently -> Check live date rn
     pass
 
 def submitChallenge():
+    ##maybe add the challenge to some sort of database -> can implement 
+    pass
+def inviteFriends():
     pass
